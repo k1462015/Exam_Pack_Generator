cd36725dd6350584f0ebc1326ece32d8c483c940
Tahmidul Islam <k1462015@kcl.ac.uk>
2015-05-11 22:27:31 +0100

    Updated look of application to match operating system.

--- EXAM_PRO/src/MainFrame.java
+++ EXAM_PRO/src/MainFrame.java
@@ -71,15 +71,8 @@ public class MainFrame extends JFrame{
 	public MainFrame(){
 		super("Exam-Pro (Trial)");
 		try {
-		    for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
-		        if ("Nimbus".equals(info.getName())) {
-		            UIManager.setLookAndFeel(info.getClassName());
-		            break;
-		        }
-		    }
-		} catch (Exception e) {
-		    // If Nimbus is not available, you can set the GUI to another look and feel.
-		}
+	        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+	    } catch (Exception e) {e.printStackTrace();     }
 		initUi();
 	}
 	
